6d7e1ab0cd4f763ba40df560d2d202ea41ffbb45
ReferralsLeada <support@leada.in>
2025-07-09 18:35:17 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
    server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://referrals-qp8h.onrender.com',
         changeOrigin: true,
       },
     },
