54701e3144f3cb308653a08c75d07e62511a554d
leada2025 <webdevldd@gmail.com>
2025-07-10 16:45:21 +0530

    mongo

--- backend/server.js
+++ backend/server.js
@@ -49,10 +49,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(DB)
 .then(() => console.log('✅ DB connection successful!'))
 .catch(err => console.error('❌ DB connection error:', err));
 
